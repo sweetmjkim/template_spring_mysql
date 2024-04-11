6f879dcf8f011102c51a9904a1a60145d79c18cf
sweetmjkim <sweet.mjkim@gmail.com>
2024-04-11 06:46:12 +0000

    QuestBoardController

--- src/main/java/com/example/co_templates/quests/restapis/QuestBoardController.java
+++ src/main/java/com/example/co_templates/quests/restapis/QuestBoardController.java
@@ -1,5 +1,42 @@
 package com.example.co_templates.quests.restapis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.co_templates.quests.services.QuestBoardService;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
 public class QuestBoardController {
-    
+
+    @Autowired
+    QuestBoardService boardService;
+
+    @GetMapping("/q/r/board/list/{pageNumber}")
+    public ResponseEntity<List<HashMap<String, Object>>> list(@PathVariable Integer pageNumber) {
+        List<HashMap<String, Object>> itemList = boardService.list(pageNumber);
+        return ResponseEntity.ok().body(itemList);
+    }
+
+    @GetMapping("/q/r/board/view/{pk_id}")
+    public ResponseEntity<HashMap<String, Object>> view(@PathVariable("pk_id") Integer pkId) {
+        HashMap<String, Object> itemDetails = boardService.view(pkId);
+        return ResponseEntity.ok().body(itemDetails);
+    }
+
+    @DeleteMapping("/q/r/board/delete/{pk_id}")
+    public ResponseEntity<Integer> delete(@PathVariable("pk_id") Integer pkId) {
+        int result = boardService.delete(pkId);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping("/q/r/board/insert/{title}/{contents}")
+    public ResponseEntity<Integer> insert(@PathVariable("title") String title,
+            @PathVariable("contents") String contents) {
+        int result = boardService.insert(title, contents);
+        return ResponseEntity.ok().body(result);
+    }
 }
--- src/main/java/com/example/co_templates/quests/services/QuestBoardService.java
+++ src/main/java/com/example/co_templates/quests/services/QuestBoardService.java
@@ -1,5 +1,54 @@
 package com.example.co_templates.quests.services;
 
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
 public class QuestBoardService {
+
+    final AtomicInteger pkIdCounter = new AtomicInteger(); 
+    final List<HashMap<String, Object>> boardItems = new CopyOnWriteArrayList<>(); 
+
+    public QuestBoardService() {
+        insert("무한의 대검", "치명타 피해량 보너스를 부여하며, 치명타 확률을 증가시킵니다.");
+        insert("가시 갑옷", "공격자에게 피해를 반사하며, 공격자의 치유 효과를 감소시킵니다.");
+        insert("라바돈의 죽음모자", "주문력을 대폭 증가시켜 능력치를 강화합니다.");
+    }
+
+    public List<HashMap<String, Object>> list(Integer pageNumber) {
+        return boardItems;
+    }
+
+    public HashMap<String, Object> view(Integer pkId) {
+        for (HashMap<String, Object> item : boardItems) {
+            if (item.get("PK_ID").equals(pkId)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public int delete(Integer pkId) {
+        for (HashMap<String, Object> item : boardItems) {
+            if (item.get("PK_ID").equals(pkId)) {
+                boardItems.remove(item);
+                return 1;
+            }
+        }
+        return 0;
+    }
     
+
+    public int insert(String title, String contents) {
+        int newPkId = pkIdCounter.incrementAndGet();
+        HashMap<String, Object> newItem = new HashMap<>();
+        newItem.put("PK_ID", newPkId);
+        newItem.put("TITLE", title);
+        newItem.put("CONTENTS", contents);
+        boardItems.add(newItem);
+        return newPkId;
+    }
 }
